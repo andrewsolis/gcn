eed9c4f161874db831f864eff34dcb2d82979aea
Andrew Solis <ajs2987@c228-105.maverick2.tacc.utexas.edu>
2022-04-07 13:16:49 -0500

    update to v2 tensorflow

---
+++ gcn_v2/models_parallel.py
@@ -0,0 +1,204 @@
+from gcn.layers import *
+from gcn.metrics import *
+
+import horovod.tensorflow as hvd
+
+flags = tf.compat.v1.app.flags
+FLAGS = flags.FLAGS
+
+
+class Model(object):
+    def __init__(self, **kwargs):
+        allowed_kwargs = {'name', 'logging'}
+        for kwarg in kwargs.keys():
+            assert kwarg in allowed_kwargs, 'Invalid keyword argument: ' + kwarg
+        name = kwargs.get('name')
+        if not name:
+            name = self.__class__.__name__.lower()
+        self.name = name
+
+        logging = kwargs.get('logging', False)
+        self.logging = logging
+
+        self.vars = {}
+        self.placeholders = {}
+
+        self.layers = []
+        self.activations = []
+
+        self.inputs = None
+        self.outputs = None
+
+        self.loss = 0
+        self.accuracy = 0
+        self.optimizer = None
+        self.opt_op = None
+
+        # horovod varaibles for model
+        self.checkpoint_dir = None
+        self.config = None
+        self.hooks = None
+
+    def _build(self):
+        raise NotImplementedError
+
+    def build(self):
+        """ Wrapper for _build() """
+        with tf.compat.v1.variable_scope(self.name):
+            self._build()
+
+        # Build sequential layer model
+        self.activations.append(self.inputs)
+        for layer in self.layers:
+            hidden = layer(self.activations[-1])
+            self.activations.append(hidden)
+        self.outputs = self.activations[-1]
+
+        # Store model variables for easy access
+        variables = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.GLOBAL_VARIABLES, scope=self.name)
+        self.vars = {var.name: var for var in variables}
+
+        # Build metrics
+        self._loss()
+        self._accuracy()
+
+        self.opt_op = self.optimizer.minimize(self.loss)
+
+        # Save checkpoints only on worker 0 to prevent other workers from corrupting them.
+        self.checkpoint_dir = '/tmp/train_logs' if hvd.rank() == 0 else None
+
+    def predict(self):
+        pass
+
+    def _loss(self):
+        raise NotImplementedError
+
+    def _accuracy(self):
+        raise NotImplementedError
+
+    def save(self, sess=None):
+        if not sess:
+            raise AttributeError("TensorFlow session not provided.")
+        saver = tf.compat.v1.train.Saver(self.vars)
+        save_path = saver.save(sess, "tmp/%s.ckpt" % self.name)
+        print("Model saved in file: %s" % save_path)
+
+    def load(self, sess=None):
+        if not sess:
+            raise AttributeError("TensorFlow session not provided.")
+        saver = tf.compat.v1.train.Saver(self.vars)
+        save_path = "tmp/%s.ckpt" % self.name
+        saver.restore(sess, save_path)
+        print("Model restored from file: %s" % save_path)
+
+
+class MLP(Model):
+    def __init__(self, placeholders, input_dim, **kwargs):
+        super(MLP, self).__init__(**kwargs)
+
+        self.inputs = placeholders['features']
+        self.input_dim = input_dim
+        # self.input_dim = self.inputs.get_shape().as_list()[1]  # To be supported in future Tensorflow versions
+        self.output_dim = placeholders['labels'].get_shape().as_list()[1]
+        self.placeholders = placeholders
+
+        self.optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
+
+        self.build()
+
+    def _loss(self):
+        # Weight decay loss
+        for var in self.layers[0].vars.values():
+            self.loss += FLAGS.weight_decay * tf.nn.l2_loss(var)
+
+        # Cross entropy error
+        self.loss += masked_softmax_cross_entropy(self.outputs, self.placeholders['labels'],
+                                                  self.placeholders['labels_mask'])
+
+    def _accuracy(self):
+        self.accuracy = masked_accuracy(self.outputs, self.placeholders['labels'],
+                                        self.placeholders['labels_mask'])
+
+    def _build(self):
+        self.layers.append(Dense(input_dim=self.input_dim,
+                                 output_dim=FLAGS.hidden1,
+                                 placeholders=self.placeholders,
+                                 act=tf.nn.relu,
+                                 dropout=True,
+                                 sparse_inputs=True,
+                                 logging=self.logging))
+
+        self.layers.append(Dense(input_dim=FLAGS.hidden1,
+                                 output_dim=self.output_dim,
+                                 placeholders=self.placeholders,
+                                 act=lambda x: x,
+                                 dropout=True,
+                                 logging=self.logging))
+
+    def predict(self):
+        return tf.nn.softmax(self.outputs)
+
+
+class GCN(Model):
+    def __init__(self, placeholders, input_dim, **kwargs):
+        super(GCN, self).__init__(**kwargs)
+
+        # initialize horovod
+        hvd.init()
+
+        # Pin GPU to be used to process local rank (one GPU per process)
+        self.config = tf.compat.v1.ConfigProto()
+        self.config.gpu_options.visible_device_list = str(hvd.local_rank())
+
+        self.inputs = placeholders['features']
+        self.input_dim = input_dim
+        # self.input_dim = self.inputs.get_shape().as_list()[1]  # To be supported in future Tensorflow versions
+        self.output_dim = placeholders['labels'].get_shape().as_list()[1]
+        self.placeholders = placeholders
+
+        print( 'horovod size:', hvd.size())
+
+        # multiple learning rate by horovod size
+        self.optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=(FLAGS.learning_rate * hvd.size() ) )
+
+        # add horovod distributed optimizer
+        self.optimizer = hvd.DistributedOptimizer( self.optimizer )
+
+        # Add hook to broadcast variables from rank 0 to all other processes during
+        # initialization.
+        self.hooks = [hvd.BroadcastGlobalVariablesHook(0)]        
+
+        self.build()
+
+    def _loss(self):
+        # Weight decay loss
+        for var in self.layers[0].vars.values():
+            self.loss += FLAGS.weight_decay * tf.nn.l2_loss(var)
+
+        # Cross entropy error
+        self.loss += masked_softmax_cross_entropy(self.outputs, self.placeholders['labels'],
+                                                  self.placeholders['labels_mask'])
+
+    def _accuracy(self):
+        self.accuracy = masked_accuracy(self.outputs, self.placeholders['labels'],
+                                        self.placeholders['labels_mask'])
+
+    def _build(self):
+
+        self.layers.append(GraphConvolution(input_dim=self.input_dim,
+                                            output_dim=FLAGS.hidden1,
+                                            placeholders=self.placeholders,
+                                            act=tf.nn.relu,
+                                            dropout=True,
+                                            sparse_inputs=True,
+                                            logging=self.logging))
+
+        self.layers.append(GraphConvolution(input_dim=FLAGS.hidden1,
+                                            output_dim=self.output_dim,
+                                            placeholders=self.placeholders,
+                                            act=lambda x: x,
+                                            dropout=True,
+                                            logging=self.logging))
+
+    def predict(self):
+        return tf.nn.softmax(self.outputs)
--- setup.py
+++ setup.py
@@ -10,9 +10,27 @@ setup(name='gcn',
       download_url='https://github.com/tkipf/gcn',
       license='MIT',
       install_requires=['numpy>=1.15.4',
-                        'tensorflow>=1.15.2,<2.0',
+                        'tensorflow>=1.15.2',
                         'networkx>=2.2',
                         'scipy>=1.1.0'
                         ],
       package_data={'gcn': ['README.md']},
       packages=find_packages())
+
+
+setup(name='gcn_v2',
+      version='1.0',
+      description='Graph Convolutional Networks in Tensorflow',
+      author='Thomas Kipf',
+      author_email='thomas.kipf@gmail.com',
+      url='https://tkipf.github.io',
+      download_url='https://github.com/tkipf/gcn',
+      license='MIT',
+      install_requires=['numpy>=1.15.4',
+                        'tensorflow>=2.1.0',
+                        'networkx>=2.2',
+                        'scipy>=1.1.0'
+                        ],
+      package_data={'gcn_v2': ['README.md']},
+      packages=find_packages())
+
